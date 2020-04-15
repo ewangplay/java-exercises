bf8a7fd1e7ca352d76f60302e35699b90ee43f3c
ewangplay <ewangplay@gmail.com>
2020-04-15 10:03:56 +0800

    add multi-thread sample

---
+++ multithread/Test.java
@@ -0,0 +1,22 @@
+// 参考来源：https://www.cnblogs.com/hapjin/p/4678773.html
+public class Test implements Runnable{
+    private String name;
+
+    private static MethodSync methodSync = new MethodSync();
+    
+    public Test(String name){
+        this.name = name;
+    }
+    
+    @Override
+    public void run() {
+        methodSync.method(name);
+    }
+    
+    public static void main(String[] args) {
+        Thread t1 = new Thread(new Test("test 1"));
+        Thread t2 = new Thread(new Test("test 2"));
+        t1.start();
+        t2.start();
+    }
+}
